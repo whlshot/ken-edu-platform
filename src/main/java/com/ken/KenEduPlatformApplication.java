9f2cd3fda8580be51cab6a65e3281b51dc28bc8e
yao_hqi <yao_hqi@example.com>
2019-08-29 17:54:06 +0800

    Initial commit

---
+++ src/main/java/com/ken/KenEduPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenEduPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenEduPlatformApplication.class, args);
+    }
+
+}
